8931823f5f866392666c26e2047af4864f03ee76
devGiovanni95 <giovanni.DevJava@hotmail.com>
2021-05-20 21:01:38 -0300

    Criando tela principal

---
+++ campo-minado-swing/src/br/com/giovanni/cm/visao/PainelTabuleiro.java
@@ -0,0 +1,13 @@
+package br.com.giovanni.cm.visao;
+
+import br.com.giovanni.cm.modelo.Tabuleiro;
+
+import javax.swing.*;
+
+
+public class PainelTabuleiro extends JPanel {
+
+    public PainelTabuleiro(Tabuleiro tabuleiro) {
+
+    }
+}
